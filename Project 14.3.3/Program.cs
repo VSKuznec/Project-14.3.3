550f3a1bec190b7713a123386dd399c846462f01
VS_KUZNEC <sevactet.games@gmail.com>
2023-08-14 23:32:33 +0300

    Добавьте файлы проекта.

---
+++ Project 14.3.3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Project_14._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedContacts = phoneBook
+                .OrderBy(s => s.Name)
+                .ThenBy(s => s.LastName);
+
+            Console.WriteLine("Введите страницу: // пример: 1, 2, 3");
+
+            while (true)
+            {
+                var input = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                else
+                {
+                    var pageContent = sortedContacts.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber + " " + entry.Email);
+
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+    public class Contact 
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
